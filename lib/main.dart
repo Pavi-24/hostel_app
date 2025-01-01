5700ea7565057942c9a6bf4f8f52590f09b1ccd6
Pavithra <pavisentil@gmail.com>
2025-01-01 15:39:08 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hostel_app/pages/HomePage.dart';
+import 'firebase_options.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hostel app',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('Hostel App',
+          style: TextStyle(
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.00),
+                    border: Border.all(
+                        color: Colors.black
+                    ),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height*0.2,
+                  child: Text('Rollcall'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.00),
+                    border: Border.all(
+                        color: Colors.black
+                    ),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height*0.2,
+                  child: Text('Dhobi'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(child: Text('Welcome',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0
+                ))),
+            ListTile(
+              title: Text('Settings'),
+            ),
+            ListTile(
+              title: Text('Contact us'),
+            ),
+            ListTile(
+              title: Text('About us'),
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
